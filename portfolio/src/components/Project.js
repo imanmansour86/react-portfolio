d8d6b1b6574e659a3f913950a80c2ec68b79d879
imanmansour86 <imanmansour86@gmail.com>
2022-03-01 23:11:34 -0800

    adding fonts for entire body and fixing fa icons

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -4,25 +4,24 @@ const Project = (props) => {
   return (
     <div className="container">
       {props.projects.map((project) => (
-        <div key={project.id} className="row">
-          <div className="col-md-8 .col-xs-12">
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
+        <div>
+          <h3>{project.title}</h3>
+          <div key={project.id} className="row">
+            <div className="col-md-4 .col-xs-12">
+              <p>{project.description}</p>
 
-            <a href={project.repoLink} className="card-link" target="_blank">
-              {" "}
-              <i className="fa fa-github"></i>Github{" "}
-            </a>
-            <br />
-            <a href="#" className="card-link">
-              <i className="fa-solid fa-browser"></i> Deployed
-            </a>
-          </div>
-          <div className="col-md-4 .col-xs-6">
-            <img
-              width="100%"
-              src="https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg"
-            />
+              <a href={project.repoLink} className="card-link" target="_blank">
+                {" "}
+                <i className="fa fa-github"></i> &nbsp;Github{" "}
+              </a>
+              <br />
+              <a href="#" className="card-link">
+                <i class="fa fa-desktop"></i>&nbsp; Deployed
+              </a>
+            </div>
+            <div className="col-md-8 .col-xs-6">
+              <img src="https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg" />
+            </div>
           </div>
         </div>
       ))}
