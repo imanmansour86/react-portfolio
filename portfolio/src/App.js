162f3c89a4c29a11a55577e59a405897a26a6354
imanmansour86 <imanmansour86@gmail.com>
2022-03-01 22:22:46 -0800

    passing projects object to project component as props

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,11 +4,32 @@ import "./App.css";
 import Header from "./components/Header";
 import Project from "./components/Project";
 
+const projects = [
+  {
+    id: 1,
+    title: "Social Network API",
+    description:
+      "API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. Built using Express.js, MongoDB, and Mongoose ODM",
+    repoLink: "https://github.com/imanmansour86/social-network-API",
+    demoLink:
+      "https://drive.google.com/file/d/13MybBc7J8yTOBMMDrfyQrGuwH1rOQ4CJ/view",
+  },
+  {
+    id: 2,
+    title: "Social Network API",
+    description:
+      "API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. Built using Express.js, MongoDB, and Mongoose ODM",
+    repoLink: "https://github.com/imanmansour86/social-network-API",
+    demoLink:
+      "https://drive.google.com/file/d/13MybBc7J8yTOBMMDrfyQrGuwH1rOQ4CJ/view",
+  },
+];
+
 function App() {
   return (
     <div>
       <Header />
-      <Project />
+      <Project projects={projects} />
     </div>
   );
 }
--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,34 +1,31 @@
 import React from "react";
 
-const Project = () => {
+const Project = (props) => {
   return (
-    <div class="container">
-      <div className="row">
-        <div className="col-md-8 .col-xs-12">
-          <h2>GetMoving</h2>
-          <p>
-            Large paragraph Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Exercitationem, aliquam consectetur, tenetur sint officia
-            reprehenderit maxime placeat neque debitis voluptatem dolores nisi
-            porro velit vero eos tempora possimus atque veritatis?
-          </p>
+    <div className="container">
+      {props.projects.map((project) => (
+        <div key={project.id} className="row">
+          <div className="col-md-8 .col-xs-12">
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
 
-          <a href="#" className="card-link">
-            {" "}
-            <i className="fa fa-github"></i>Github{" "}
-          </a>
-          <br />
-          <a href="#" className="card-link">
-            <i class="fa-solid fa-browser"></i> Deployed
-          </a>
+            <a href={project.repoLink} className="card-link" target="_blank">
+              {" "}
+              <i className="fa fa-github"></i>Github{" "}
+            </a>
+            <br />
+            <a href="#" className="card-link">
+              <i className="fa-solid fa-browser"></i> Deployed
+            </a>
+          </div>
+          <div className="col-md-4 .col-xs-6">
+            <img
+              width="100%"
+              src="https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg"
+            />
+          </div>
         </div>
-        <div className="col-md-4 .col-xs-6">
-          <img
-            width="100%"
-            src="https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
