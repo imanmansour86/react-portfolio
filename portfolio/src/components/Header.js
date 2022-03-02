97ee10100e5dcbfd3516a032d1dfa34842769b11
imanmansour86 <imanmansour86@gmail.com>
2022-03-01 20:46:25 -0800

    adding background image to body

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,7 +1,38 @@
 import React from "react";
+import style from "./style.css";
 
 const Header = () => {
-  return <div>test</div>;
+  return (
+    <body>
+      <div>
+        <div>
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">About Me</a>
+            </li>
+          </ul>
+        </div>
+        <div className="head">
+          <h1>Iman Mansour</h1>
+        </div>
+      </div>
+    </body>
+  );
 };
 
 export default Header;
