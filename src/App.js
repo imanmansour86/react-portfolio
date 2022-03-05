d66163d3ed41256fb812b24a54b1313d5724c19e
imanmansour86 <imanmansour86@gmail.com>
2022-03-04 22:15:47 -0800

    adding reveal animation library and adding animation to navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Zoom from "react-reveal/Zoom"; // Importing Zoom effect
 
 const App = () => <PortfolioContainer />;
 
