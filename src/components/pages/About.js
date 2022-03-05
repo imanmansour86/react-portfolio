6a4c8a50f07d0b85dfd97bbf1839bc39c93c01e4
imanmansour86 <imanmansour86@gmail.com>
2022-03-04 22:22:16 -0800

    adding animation to about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Zoom from "react-reveal/Slide";
 
 const About = () => {
   return (
@@ -7,17 +8,19 @@ const About = () => {
         <h3>About me</h3>
         <div className="row">
           <div className="col-md-8 .col-xs-12">
-            <p>
-              {" "}
-              Hello! I am a Full Stack Web Developer with work experience in
-              Quality Assurance. I have a BE in Computer Engineering and Masters
-              in Engineering Managment with a minor in Political Studies. I love
-              solving problems and working on new challenges everyday. I also
-              love to learn new skills and enjoy building user friendly
-              websites. My work experience helps me visualize the best version
-              of the app that is intuitive as well as functional. Based in San
-              Jose, CA.
-            </p>
+            <Zoom left>
+              <p>
+                {" "}
+                Hello! I am a Full Stack Web Developer with work experience in
+                Quality Assurance. I have a BE in Computer Engineering and
+                Masters in Engineering Managment with a minor in Political
+                Studies. I love solving problems and working on new challenges
+                everyday. I also love to learn new skills and enjoy building
+                user friendly websites. My work experience helps me visualize
+                the best version of the app that is intuitive as well as
+                functional. Based in San Jose, CA.
+              </p>
+            </Zoom>
           </div>
           <div className="col-md-4 .col-xs-6">
             <img className="my-image" src={require(`../../assets/iman.png`)} />
